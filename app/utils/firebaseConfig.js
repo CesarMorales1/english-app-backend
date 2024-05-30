b7a1f0bd1c5d9f56c16a2f27019effc80920df48
EmmanuelChacon-ve <emmanuelechra@gmail.com>
2024-05-29 23:19:12 -0400

    mejorado el codigo

--- app/utils/firebaseConfig.js
+++ app/utils/firebaseConfig.js
@@ -1,8 +1,6 @@
 import { Storage } from "@google-cloud/storage";
 import { v4 as uuidv4 } from "uuid";
 
-const uuid = uuidv4();
-
 const storage = new Storage({
   projectId: "english-plis",
   keyFilename: "./serviceAccountKey.json",
@@ -10,4 +8,39 @@ const storage = new Storage({
 
 const bucket = storage.bucket("english-plis.appspot.com");
 
-export { bucket };
+const uploadFileToFirebase = (file) => {
+  return new Promise((resolve, reject) => {
+    const blob = bucket.file(`${uuidv4()}-${file.originalname}`);
+    const blobStream = blob.createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
+
+    blobStream.on("error", (err) => {
+      reject(err);
+    });
+
+    blobStream.on("finish", async () => {
+      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+        bucket.name
+      }/o/${encodeURIComponent(blob.name)}?alt=media`;
+      const token = uuidv4();
+
+      try {
+        await blob.setMetadata({
+          metadata: {
+            firebaseStorageDownloadTokens: token,
+          },
+        });
+        resolve(`${publicUrl}&token=${token}`);
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    blobStream.end(file.buffer);
+  });
+};
+
+export { bucket, uploadFileToFirebase };
