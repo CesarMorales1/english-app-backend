d4288f1b9d71f7959cca9f2d90e4c981ca8115ce
EmmanuelChacon-ve <emmanuelechra@gmail.com>
2024-05-29 23:11:30 -0400

    otra forma

--- app/services/videoService.js
+++ app/services/videoService.js
@@ -22,6 +22,63 @@ export const uploadVideo = async (
     });
 
     blobStream.on("finish", async () => {
+      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+        bucket.name
+      }/o/${encodeURIComponent(blob.name)}?alt=media`;
+
+      // Generate a token for the file
+      const token = uuidv4();
+      await blob.setMetadata({
+        metadata: {
+          firebaseStorageDownloadTokens: token,
+        },
+      });
+
+      try {
+        const video = await prisma.videos.create({
+          data: {
+            titulo,
+            id_course: parseInt(id_course, 10),
+            url: `${publicUrl}&token=${token}`,
+            duration_video: duration_video || "1:00", // Valor por defecto
+            detail_video: detail_video || "Video explicativo del tema tal", // Valor por defecto
+          },
+        });
+        resolve(video);
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    blobStream.end(file.buffer);
+  });
+};
+
+/* 
+// services/videoService.js
+import { PrismaClient } from "@prisma/client";
+import { bucket } from "../utils/firebaseConfig.js";
+import { v4 as uuidv4 } from "uuid";
+
+const prisma = new PrismaClient();
+
+export const uploadVideo = async (
+  file,
+  { titulo, id_course, duration_video, detail_video }
+) => {
+  const blob = bucket.file(`${uuidv4()}-${file.originalname}`);
+  const blobStream = blob.createWriteStream({
+    metadata: {
+      contentType: file.mimetype,
+    },
+  });
+
+  return new Promise((resolve, reject) => {
+    blobStream.on("error", (err) => {
+      reject(err);
+    });
+
+    blobStream.on("finish", async () => {
       const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
 
       // Generate a token for the file
@@ -54,3 +111,4 @@ export const uploadVideo = async (
     blobStream.end(file.buffer);
   });
 };
+ */
